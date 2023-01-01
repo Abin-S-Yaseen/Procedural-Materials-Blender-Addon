1ff893dea43b0665645524fc0dbb162de58f349a
Abin-S-Yaseen <abinchingeli12@gmail.com>
2023-01-01 20:58:20 +0530

    Refining the existing code for gold and wood materials

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Procedural Materials Addon",
     "author": "Abin S Yaseen",
     "version": (1, 0),
-    "blender": (3, 0, 0),
+    "blender": (3, 4, 0),
     "location": "View3D > Toolshelf",
     "description": "A collection of procedural materials",
     "warning": "",
@@ -13,49 +13,98 @@ bl_info = {
 import bpy
 
 
-    #Operator for the Gold Shader
+#shader nodes
+principled_node = 'ShaderNodeBsdfPrincipled'
+material_output_node = 'ShaderNodeOutputMaterial'
+rgb_node = 'ShaderNodeRGB'
+texture_coordinate_node = 'ShaderNodeTexCoord'
+mapping_node = 'ShaderNodeMapping'
+musgrave_node = 'ShaderNodeTexMusgrave'
+noise_node = 'ShaderNodeTexNoise'
+colorramp_node = 'ShaderNodeValToRGB'
+bump_node = 'ShaderNodeBump'
+
+# Gold material
 class SHADER_OT_GOLD(bpy.types.Operator):
     
     bl_label = "Gold"
     bl_idname = 'shader.gold_operator'
     
+    
     def execute(self, context):
         
-            #Create a Shader Material and name it Gold
+        #Create a Shader Material and name it Gold
         material_gold = bpy.data.materials.new(name= "Gold")
         material_gold.use_nodes = True
         
-        material_output = material_gold.node_tree.nodes.get('Material Output')
+        node = material_gold.node_tree.nodes
+        node.clear()
+
+        material_output = node.new(material_output_node)
         material_output.location = (600,0)
         material_output.select = False
         
         
-            #Create reference to the RGB node      
-        rgb_node = material_gold.node_tree.nodes.new('ShaderNodeRGB')
-        rgb_node.location = (0,-100)
-        rgb_node.outputs[0].default_value = (1, 0.766, 0.336, 1)
-        rgb_node.select = False  # deselects it
-        rgb_node.hide = True  # for minimizing the node
-        
-        
-            #Create reference to the Principled Node
-        principled = material_gold.node_tree.nodes.get('Principled BSDF')
+        #Create reference to the Principled Node
+        principled = node.new(principled_node)
         principled.location = (200,0)
         principled.select = False
         principled.inputs[4].default_value = 1.0
         principled.inputs[6].default_value = 1.0
-        principled.inputs[9].default_value = 0.371
+        principled.inputs[9].default_value = 0.27
+
+        texture_coordinate = node.new(texture_coordinate_node)
+        texture_coordinate.location = (-700,0)
+        texture_coordinate.select = False
         
+        mapping = node.new(mapping_node)
+        mapping.location = (-500,0)
+        mapping.select = False
+
+        musgrave = node.new(musgrave_node)
+        musgrave.location = (-300,0)
+        musgrave.inputs[2].default_value = 400
+        musgrave.inputs[3].default_value = 16
+        musgrave.inputs[4].default_value = 0
+        musgrave.select = False
+
+        colorramp = node.new(colorramp_node)
+        colorramp.location = (-100,0)
+        colorramp.color_ramp.elements[0].color = (0.610, 0.337, 0.084, 1)
+        colorramp.color_ramp.elements[1].color = (0.279, 0.098, 0.025, 1)
+        colorramp.select = False
+
+        noise = node.new(noise_node)
+        noise.location = (-300,-300)
+        noise.inputs[2].default_value = 12
+        noise.inputs[3].default_value = 16
+        noise.inputs[4].default_value = 0.45
+        noise.inputs[5].default_value = 0
+        noise.select = False
         
-            #for connecting two boxes with a link
-        material_gold.node_tree.links.new(rgb_node.outputs[0], principled.inputs[0])
+        bump = node.new(bump_node)
+        bump.location = (-100,-300)
+        bump.inputs[0].default_value = 0.008
+        bump.select = False
+        
+        #links
+        link = material_gold.node_tree.links
+        link.new(texture_coordinate.outputs[3], mapping.inputs[0])
+        link.new(mapping.outputs[0], musgrave.inputs[0])
+        link.new(mapping.outputs[0], noise.inputs[0])
+        link.new(musgrave.outputs[0], colorramp.inputs[0])
+        link.new(noise.outputs[0], bump.inputs[2])
+        link.new(colorramp.outputs[0], principled.inputs[0])
+        link.new(bump.outputs[0], principled.inputs[22])
+        link.new(principled.outputs[0], material_output.inputs[0])
             
             
-            #Add Material to the currently selected object
+        #Add Material to the currently selected object
         bpy.context.object.active_material = material_gold
             
         return {'FINISHED'}
-    
+
+# wood material
 class SHADER_OT_WOOD(bpy.types.Operator):
     
     bl_label = "Wood"
@@ -66,22 +115,25 @@ class SHADER_OT_WOOD(bpy.types.Operator):
         material_wood = bpy.data.materials.new(name= "Wood")
         material_wood.use_nodes = True
         
-        texture_coordinate = material_wood.node_tree.nodes.new('ShaderNodeTexCoord')
-        texture_coordinate.location = (-900,0)
+        node = material_wood.node_tree.nodes
+        node.clear()
+
+        texture_coordinate = node.new(texture_coordinate_node)
+        texture_coordinate.location = (-600,0)
         texture_coordinate.select = False
         
-        mapping = material_wood.node_tree.nodes.new('ShaderNodeMapping')
-        mapping.location = (-600,0)
+        mapping = node.new(mapping_node)
+        mapping.location = (-400,0)
         mapping.inputs[3].default_value[0] = 0.45
         mapping.inputs[3].default_value[1] = 2.77
         mapping.select = False
         
-        musgrave = material_wood.node_tree.nodes.new('ShaderNodeTexMusgrave')
-        musgrave.location = (-300,0)
+        musgrave = node.new(musgrave_node)
+        musgrave.location = (-200,0)
         musgrave.inputs[2].default_value = 0.64
         musgrave.select = False
         
-        noise = material_wood.node_tree.nodes.new('ShaderNodeTexNoise')
+        noise = node.new(noise_node)
         noise.location = (0,0)
         noise.inputs[2].default_value = 2.15
         noise.inputs[3].default_value = 18
@@ -89,8 +141,8 @@ class SHADER_OT_WOOD(bpy.types.Operator):
         noise.inputs[5].default_value = 0.750
         noise.select = False
         
-        colorramp1 = material_wood.node_tree.nodes.new('ShaderNodeValToRGB')
-        colorramp1.location = (300,300)
+        colorramp1 = node.new(colorramp_node)
+        colorramp1.location = (250,300)
         colorramp1.color_ramp.elements.new(position=0.377)
         colorramp1.color_ramp.elements[2].position = 0.668
         colorramp1.color_ramp.elements[0].color = (1, 0.25, 0.058, 1) 
@@ -99,23 +151,23 @@ class SHADER_OT_WOOD(bpy.types.Operator):
         colorramp1.color_ramp.interpolation = 'EASE'
         colorramp1.select = False
         
-        colorramp2 = material_wood.node_tree.nodes.new('ShaderNodeValToRGB')
-        colorramp2.location = (300,0)
+        colorramp2 = node.new(colorramp_node)
+        colorramp2.location = (250,0)
         colorramp2.color_ramp.elements[0].position = 0.441
         colorramp2.select = False
         
-        bump = material_wood.node_tree.nodes.new('ShaderNodeBump')
-        bump.location = (300,-300)
+        bump = node.new(bump_node)
+        bump.location = (250,-300)
         bump.inputs[0].default_value = 0.143
         bump.select = False
         
-        principled = material_wood.node_tree.nodes.get('Principled BSDF')
+        principled = node.new(principled_node)
         principled.location = (600,0)
         principled.inputs[1].default_value = 0.041
         principled.select = False
         
-        material_output = material_wood.node_tree.nodes.get('Material Output')
-        material_output.location = (900,0)
+        material_output = node.new(material_output_node)
+        material_output.location = (1000,0)
         material_output.select = False
         
         
@@ -131,13 +183,14 @@ class SHADER_OT_WOOD(bpy.types.Operator):
         link.new(colorramp1.outputs[0], principled.inputs[3])
         link.new(colorramp2.outputs[0], principled.inputs[9])
         link.new(bump.outputs[0], principled.inputs[22])
+        link.new(principled.outputs[0], material_output.inputs[0])
         
                 
         bpy.context.object.active_material = material_wood
         
         return {'FINISHED'}
         
-    #creat Main Panel
+#creat Main Panel
 class MainPanel(bpy.types.Panel):
     bl_label = "Procedural Textures"
     bl_idname = "SHADER_PT_MAINPANEL"
@@ -156,13 +209,13 @@ class MainPanel(bpy.types.Panel):
         row.operator('shader.wood_operator')
         
 
-    #registering classes
+#registering classes
 def register():
     bpy.utils.register_class(MainPanel)
     bpy.utils.register_class(SHADER_OT_GOLD)
     bpy.utils.register_class(SHADER_OT_WOOD)
     
-    #unregistering classes
+#unregistering classes
 def unregister():
     bpy.utils.unregister_class(MainPanel)
     bpy.utils.unregister_class(SHADER_OT_GOLD)
